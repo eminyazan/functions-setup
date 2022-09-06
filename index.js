dacfcf5090492f21b4c21054a6624f3354eca687
eminyazan <eminyazan12@gmail.com>
2022-09-06 15:31:53 +0300

    pubsup scheduled function

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const functions = require("firebase-functions");
+const admin=require("firebase-admin")
+admin.initializeApp()
+const db=admin.firestore()
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.pubsub=functions.region("europe-west3").pubsub.schedule("20 15 * * *").timeZone("Europe/Istanbul")
+    .onRun(async (context)=>{
+
+        await db.collection("users").get().then(async (res)=>{
+            const docs=res.docs
+
+            for(const doc of docs){
+
+                const user=doc.data()
+                user.life+=10
+
+                await db.collection("users").doc(user.uid).update(user)
+
+            }
+
+        })
+
+
+    })
+
 
 exports.helloWorldTOFunctions = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase functions!");
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase functions!");
 });
+
+
+
+
+
+
